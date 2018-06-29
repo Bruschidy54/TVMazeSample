37058838227d3b93760d5eff2e5f08d928175b3d
Dylan Bruschi <dylanbruschi54@gmail.com>
2018-06-29 16:12:52 -0500

    Implemented design of EpisodesTableViewController
    Fix aspect ratio of imageView
    Implement theme colors of cells and nav bar
    Fix word wrapping of cell labels
    
    [Ticket: X]

--- TVMazeSample/CustomImageView.swift
+++
@@ -1,9 +0,0 @@
-//
-//  CustomImageView.swift
-//  TVMazeSample
-//
-//  Created by Dylan Bruschi on 6/27/18.
-//  Copyright © 2018 Dylan Bruschi. All rights reserved.
-//
-
-import Foundation
---
+++ TVMazeSample/Utils/CustomImageView.swift
@@ -0,0 +1,54 @@
+//
+//  CustomImageView.swift
+//  TVMazeSample
+//
+//  Created by Dylan Bruschi on 6/27/18.
+//  Copyright © 2018 Dylan Bruschi. All rights reserved.
+//
+
+import UIKit
+
+var imageCache = [String: UIImage]()
+
+class CustomImageView: UIImageView {
+    
+    var lastURLUsedToLoadImage: String?
+    
+    func loadImage(urlString: String) {
+        
+        lastURLUsedToLoadImage = urlString
+        
+        self.image = nil
+        
+        if let cachedImage = imageCache[urlString] {
+            self.image = cachedImage
+            return
+        }
+        
+        guard let url = URL(string: urlString) else { return }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, err) in
+            if let err = err {
+                print("Failed to fetch post image:", err)
+                return
+            }
+            
+            if url.absoluteString != self.lastURLUsedToLoadImage {
+                return
+            }
+            
+            // TO DO: check for HTTP response status of 200 meaning OK
+            
+            guard let imageData = data else { return }
+            
+            let photoImage = UIImage(data: imageData)
+            
+            imageCache[url.absoluteString] = photoImage
+            
+            DispatchQueue.main.async {
+                
+                self.image = photoImage
+            }
+            }.resume()
+    }
+}
